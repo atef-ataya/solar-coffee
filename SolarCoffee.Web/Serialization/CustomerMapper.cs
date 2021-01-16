ba799859cd281b08a8e5864c33aaf97ad1784acf
Atef Ataya <atef.ataya@gmail.com>
2021-01-16 14:27:50 +0400

    Create view models, update InventoryController

---
+++ SolarCoffee.Web/Serialization/CustomerMapper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using SolarCoffee.Data.Models;
+using SolarCoffee.Web.ViewModels;
+
+namespace SolarCoffee.Web.Serialization
+{
+    public static class CustomerMapper
+    {
+        /// <summary>
+        /// Serializes a Customer data model into a CustomerModel view model
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+       public static CustomerModel SerializeCustomer (Customer customer)
+        {
+            return new CustomerModel
+            {
+                Id = customer.Id,
+                CreatedOn = customer.CreatedOn,
+                UpdatedOn = customer.UpdatedOn,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                PrimaryAddress = MapCustomerAddress(customer.PrimaryAddress)
+            };
+        }
+        /// <summary>
+        /// Serializes a CustomerModel view model into a customer data model
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public static Customer SerializeCustomer (CustomerModel customer)
+        {
+           
+
+            return new Customer
+            {
+                CreatedOn = customer.CreatedOn,
+                UpdatedOn = customer.UpdatedOn,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                PrimaryAddress = MapCustomerAddress(customer.PrimaryAddress)
+            };
+        }
+        /// <summary>
+        /// Maps a CustomerAddress data model to a CustomerAddressModel view model
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public static CustomerAddressModel MapCustomerAddress (CustomerAddresses address)
+        {
+            return new CustomerAddressModel
+            {
+                Id = address.Id,
+                AddressLine1 = address.AddressLine1,
+                AddressLine2 = address.AddressLine2,
+                City = address.City,
+                State = address.State,
+                PostalCode = address.PostalCode,
+                Country = address.Country,
+                CreatedOn = DateTime.UtcNow,
+                UpdatedOn = DateTime.UtcNow
+            };
+        }
+
+        /// <summary>
+        /// Maps a CustomerAddress viewmodel  to a CustomerAddressModel data model
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public static CustomerAddresses MapCustomerAddress(CustomerAddressModel address)
+        {
+            return new CustomerAddresses
+            {
+                AddressLine1 = address.AddressLine1,
+                AddressLine2 = address.AddressLine2,
+                City = address.City,
+                State = address.State,
+                PostalCode = address.PostalCode,
+                Country = address.Country,
+                CreatedOn = DateTime.UtcNow,
+                UpdatedOn = DateTime.UtcNow
+            };
+        }
+    }
+}
---
+++ SolarCoffee.Web/Serialization/OrderMapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using SolarCoffee.Data.Models;
+using System.Linq;
+using SolarCoffee.Web.ViewModels;
+using System.Collections.Generic;
+
+namespace SolarCoffee.Web.Serialization
+{
+    /// <summary>
+    /// Handles mapping Order data models to and from related View Models
+    /// </summary>
+    public static class OrderMapper
+    {
+        /// <summary>
+        /// Maps an InvoiceModel view model to a SalesOrder data model
+        /// </summary>
+        /// <param name="invoice"></param>
+        /// <returns></returns>
+        public static SalesOrder SerializeInvoiceToOrder (InvoiceModel invoice)
+        {
+            var salesOrderItems = invoice.LineItems
+                .Select(item => new SalesOrderItem
+                {
+                    Id = item.Id,
+                    Quantity = item.Quantity,
+                    Product = ProductMapper.SerializeProductModel(item.Product)
+                }).ToList();
+
+            return new SalesOrder
+            {
+                SalesOrderItems = salesOrderItems,
+                CreatedOn = DateTime.UtcNow,
+                UpdatedOn = DateTime.UtcNow
+            };
+        }
+        /// <summary>
+        /// maps a collection of salesorders to Order
+        /// </summary>
+        /// <param name="orders"></param>
+        /// <returns></returns>
+        public static List<OrderModel> SerializeOrdersToViewModels(IEnumerable<SalesOrder> orders)
+        {
+            return orders.Select(order => new OrderModel
+            {
+                Id = order.Id,
+                CreatedOn = order.CreatedOn,
+                UpdatedOn = order.UpdatedOn,
+                SalesOrderItems = SerializeSalesOrderItems(order.SalesOrderItems),
+                Customer = CustomerMapper.SerializeCustomer(order.Customer),
+                IsPaid = order.IsPaid
+            }).ToList();
+        }
+        /// <summary>
+        /// Maps a collection of SalesOrderItems (data) to SalesOrderItemsModels (viewModels)
+        /// </summary>
+        /// <param name="orderItems"></param>
+        /// <returns></returns>
+        private static List<SalesOrderItemModel> SerializeSalesOrderItems(IEnumerable<SalesOrderItem> orderItems)
+        {
+            return orderItems.Select(item => new SalesOrderItemModel {
+                Id = item.Id,
+                Quantity = item.Quantity,
+                Product = ProductMapper.SerializeProductModel(item.Product)
+            }).ToList();
+        }
+    }
+}
